b0fc5323b12aacb380ed7e46d4efa2507c651afa
Kacper Piasecki <k.r.piasecki2@gmail.com>
2024-04-03 00:12:58 +0200

    Basic backend business logic implemented.

--- src/main/java/tech/getarrays/employeemanager/repository/EmployeeRepository.java
+++ src/main/java/tech/getarrays/employeemanager/repository/EmployeeRepository.java
@@ -6,6 +6,7 @@ import tech.getarrays.employeemanager.model.Employee;
 import java.util.Optional;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    void deleteEmployeeById(Long id);
 
     Optional<Employee> findEmployeeById(Long id);
 }
